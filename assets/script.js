3c7b9c40588edf580c3d0c6b5b3dd82a77c313fe
Martin Farr <martinfarr24@gmail.com>
2023-05-22 10:43:38 -0500

    added js css and html basics

---
+++ assets/script.js
@@ -0,0 +1,136 @@
+
+
+const quizArry = [
+    {
+        id: "1"
+    question: "What is URL?"
+    options: [
+        "Uniform Resource Locators",
+        "Uniform Resource Libraries",
+        "Unicorn Reptile Lizard",
+        "Uniform Repository Libraries",
+    ],
+        correct: "Uniform Resource Locators",
+
+    },
+
+    {
+        id: "2"
+        question: "What is meta?"
+        options: [
+        "Zucker Punch",
+        "Data in space",
+        "Data about data",
+        "Teemo in lol",
+        ],
+        correct: "Datat about data"
+    },
+
+    { id: "3"
+    question: "What is dom?",
+    options: [
+        "Document Object Model",
+        "Document Obstical Monster",
+        "Designated Operation Model",
+        "Designated Open Modem",
+    ],
+    correct: "Document object model"
+    },
+
+    {
+        id: "4"
+        question: "What is API?"
+        options: [
+        "Application Project Integration",
+        "Application Programming Interface",
+        "Another Program on the Internet",
+        "Alien Project Interrogation",
+        ],
+        correct: "Application Programming Interface"
+    },
+
+    {
+        id: "5"
+        question: "What is HTTP?"
+        options: [
+        "Hyper Text Transfer Protocol",
+        "Hyper Text Tranmision Protocol",
+        "Hyper Text Transformer Protocol",
+        "Hyper Text Terrestrial Protocol",
+        ],
+        correct: "Hyper Text Transfer Protocol"
+    },
+
+    {
+        id: "6"
+        question: "What is CSS?"
+        options: [
+        "Cascading Style Sheets",
+        "Counting Style Sheets",
+        "Counting Simple Sheets",
+        "Carpenter Steak Sandwich",
+        ],
+        correct: "Cascading Style Sheets"
+    },
+
+    {
+        id: "7"
+        question: "What is NaN?"
+        options: [
+        "Short hand for nanny",
+        "Non applicable Note",
+        "Not a Number",
+        "Never again Never",
+        ],
+        correct: "Not a Number"
+    },
+
+    {
+        id: "8"
+        question: "What is meta?"
+        options: [
+        "Zucker Punch",
+        "Data in space",
+        "Data about data",
+        "Teemo in lol",
+        ],
+        correct: "Datat about data"
+    },
+
+    {
+        id: "9"
+        question: "What defines a string?"
+        options: [
+        "Quotations",
+        "Parentheses",
+        "Carrots",
+        "Curly Brackets",
+        ],
+        correct: "Quotations"
+    },
+
+    {
+        id: "10"
+        question: "What is Null?"
+        options: [
+        "Lack of any value",
+        "Gandhi's caloric intake",
+        "Money wasted by government",
+        "All of the above",
+        ],
+        correct: "Lack of any value"
+    },
+]
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
