5b3cbe6170d594497c96f63016bb28cfca663650
Martin Farr <martinfarr24@gmail.com>
2023-05-22 13:01:59 -0500

    added a bunch of javascript

--- assets/script.js
+++ assets/script.js
@@ -1,126 +1,164 @@
-
+var start = document.querySelector(".start-btn")
 
 const quizArry = [
     {
-        id: "1"
-    question: "What is URL?"
-    options: [
-        "Uniform Resource Locators",
-        "Uniform Resource Libraries",
-        "Unicorn Reptile Lizard",
-        "Uniform Repository Libraries",
-    ],
+        id: "1",
+        question: "What is URL?",
+        options: [
+            "Uniform Resource Locators",
+            "Uniform Resource Libraries",
+            "Unicorn Reptile Lizard",
+            "Uniform Repository Libraries",
+        ],
         correct: "Uniform Resource Locators",
 
     },
 
     {
-        id: "2"
-        question: "What is meta?"
+        id: "2",
+        question: "What is meta?",
         options: [
-        "Zucker Punch",
-        "Data in space",
-        "Data about data",
-        "Teemo in lol",
+            "Zucker Punch",
+            "Data in space",
+            "Data about data",
+            "Teemo in lol",
         ],
         correct: "Datat about data"
     },
 
-    { id: "3"
-    question: "What is dom?",
-    options: [
-        "Document Object Model",
-        "Document Obstical Monster",
-        "Designated Operation Model",
-        "Designated Open Modem",
-    ],
-    correct: "Document object model"
+    {
+        id: "3",
+        question: "What is dom?",
+        options: [
+            "Document Object Model",
+            "Document Obstical Monster",
+            "Designated Operation Model",
+            "Designated Open Modem",
+        ],
+        correct: "Document object model"
     },
 
     {
-        id: "4"
-        question: "What is API?"
+        id: "4",
+        question: "What is API?",
         options: [
-        "Application Project Integration",
-        "Application Programming Interface",
-        "Another Program on the Internet",
-        "Alien Project Interrogation",
+            "Application Project Integration",
+            "Application Programming Interface",
+            "Another Program on the Internet",
+            "Alien Project Interrogation",
         ],
         correct: "Application Programming Interface"
     },
 
     {
-        id: "5"
-        question: "What is HTTP?"
+        id: "5",
+        question: "What is HTTP?",
         options: [
-        "Hyper Text Transfer Protocol",
-        "Hyper Text Tranmision Protocol",
-        "Hyper Text Transformer Protocol",
-        "Hyper Text Terrestrial Protocol",
+            "Hyper Text Transfer Protocol",
+            "Hyper Text Tranmision Protocol",
+            "Hyper Text Transformer Protocol",
+            "Hyper Text Terrestrial Protocol",
         ],
         correct: "Hyper Text Transfer Protocol"
     },
 
     {
-        id: "6"
-        question: "What is CSS?"
+        id: "6",
+        question: "What is CSS?",
         options: [
-        "Cascading Style Sheets",
-        "Counting Style Sheets",
-        "Counting Simple Sheets",
-        "Carpenter Steak Sandwich",
+            "Cascading Style Sheets",
+            "Counting Style Sheets",
+            "Counting Simple Sheets",
+            "Carpenter Steak Sandwich",
         ],
         correct: "Cascading Style Sheets"
     },
 
     {
-        id: "7"
-        question: "What is NaN?"
+        id: "7",
+        question: "What is NaN?",
         options: [
-        "Short hand for nanny",
-        "Non applicable Note",
-        "Not a Number",
-        "Never again Never",
+            "Short hand for nanny",
+            "Non applicable Note",
+            "Not a Number",
+            "Never again Never",
         ],
         correct: "Not a Number"
     },
 
     {
-        id: "8"
-        question: "What is meta?"
+        id: "8",
+        question: "What is meta?",
         options: [
-        "Zucker Punch",
-        "Data in space",
-        "Data about data",
-        "Teemo in lol",
+            "Zucker Punch",
+            "Data in space",
+            "Data about data",
+            "Teemo in lol",
         ],
         correct: "Datat about data"
     },
 
     {
-        id: "9"
-        question: "What defines a string?"
+        id: "9",
+        question: "What defines a string?",
         options: [
-        "Quotations",
-        "Parentheses",
-        "Carrots",
-        "Curly Brackets",
+            "Quotations",
+            "Parentheses",
+            "Carrots",
+            "Curly Brackets",
         ],
         correct: "Quotations"
     },
 
     {
-        id: "10"
-        question: "What is Null?"
+        id: "10",
+        question: "What is Null?",
         options: [
-        "Lack of any value",
-        "Gandhi's caloric intake",
-        "Money wasted by government",
-        "All of the above",
+            "Lack of any value",
+            "Gandhi's caloric intake",
+            "Money wasted by government",
+            "All of the above",
         ],
         correct: "Lack of any value"
     },
 ]
+
+start.addEventListener("click", function () {
+    // start will begin quiz
+    startQuiz()
+    // starts timer
+    // hides start button and timer
+
+})
+
+var quizIndex = 0
+
+function startQuiz() {
+    document.querySelector("#question").textContent = quizArry[quizIndex].question
+    document.querySelector("#awnser-buttons").innerHTML = ""
+    for (var i = 0; i < 4; i++) {
+        var button = document.createElement("button");
+        button.textContent = quizArry[quizIndex].options[i]
+        button.setAttribute("class", "btn")
+        button.onclick = checkAwnser
+        document.querySelector("#awnser-buttons").append(button)
+    }
+}
+
+function checkAwnser() {
+    console.log(this)
+    // if else statment if awnser is correct
+    if (quizArry[quizIndex].correct===this.textContent) {
+        
+    } else {
+        
+    }
+    // make quizIndex go up 1
+    quizIndex ++
+    // execute start quiz
+    startQuiz()
+}
+
 
 
 // GIVEN I am taking a code quiz
